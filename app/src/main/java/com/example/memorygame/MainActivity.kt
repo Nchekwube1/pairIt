7f0d1f2f564abc3dab53cce78d800354cdbc459c
Unekwe Francis <unekwenchekwube@gmail.com>
2022-04-26 16:53:01 +0100

    set menu and alert items with handlers

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.memorygame.models.BoardSize
-import com.example.memorygame.models.DEFAULT_ICONS
-import com.example.memorygame.models.MemoryCard
 import com.example.memorygame.models.MemoryGame
 import com.google.android.material.snackbar.Snackbar
 
@@ -22,30 +22,50 @@ class MainActivity : AppCompatActivity() {
 private  lateinit var  clRoot:ConstraintLayout
 private  lateinit var movesTV:TextView
     private  lateinit var pairsTV:TextView
-private var moves:Int = 0
+    private lateinit var itemsRV:RecyclerView
+    private var clicks:Int = 0
+
+    private var moves:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
          movesTV = findViewById<TextView>(R.id.tv_moves)
          pairsTV = findViewById<TextView>(R.id.tv_pairs)
-        val itemsRV = findViewById<RecyclerView>(R.id.rv_card_items)
+         itemsRV = findViewById<RecyclerView>(R.id.rv_card_items)
    clRoot = findViewById(R.id.clRoot)
+        setupBoard()
 
-         memoryGame = MemoryGame(boardSize)
+    }
 
-        itemsRV.layoutManager = GridLayoutManager(this, boardSize.getWidth())
-       adapter= MemoryBoardAdapter(this,boardSize, memoryGame.cards,
-         object :MemoryBoardAdapter.CardClickedListener{
-             override fun onCardClicked(position: Int) {
-                 updateGameOnFlip(position)
-                 moves++
-                 movesTV.text = "Moves: $moves"
-             }
-         }
-            )
-        itemsRV.adapter = adapter
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return  true
+    }
 
-        itemsRV.setHasFixedSize(true)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mi_refresh ->{
+                if (moves > 0 && !memoryGame.hasWonGame()){
+                    showAlertDialog("Quit current game?",null,View.OnClickListener {
+                        setupBoard()
+                    })
+                }else{
+                    setupBoard()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAlertDialog(title:String, view:View?,positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title).
+            setView(view)
+            .setNegativeButton("Cancel",null)
+            .setPositiveButton("Ok"){_,_ ->
+                positiveClickListener.onClick(null)
+
+            }.show()
     }
 
     private fun updateGameOnFlip(position: Int) {
@@ -59,10 +79,34 @@ private var moves:Int = 0
 
             return
         }
-        memoryGame.flipCard(position)
+        if (memoryGame.flipCard(position)){
+            if(memoryGame.hasWonGame()){
+                Snackbar.make(clRoot,"Game Won Congratulations!!!", Snackbar.LENGTH_LONG).show()
+
+            }
+        }
         pairsTV.text = "Pairs: ${memoryGame.numPairs} / ${boardSize.getNumPairs()}"
 
         adapter.notifyDataSetChanged()
+    }
+
+    private fun setupBoard(){
+        memoryGame = MemoryGame(boardSize)
+
+        itemsRV.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+        adapter= MemoryBoardAdapter(this,boardSize, memoryGame.cards,
+            object :MemoryBoardAdapter.CardClickedListener{
+                override fun onCardClicked(position: Int) {
+                    updateGameOnFlip(position)
+                    clicks++
+                    moves = clicks / 2
+                    movesTV.text = "Moves: $moves"
+                }
+            }
+        )
+        itemsRV.adapter = adapter
+
+        itemsRV.setHasFixedSize(true)
     }
 
 }
